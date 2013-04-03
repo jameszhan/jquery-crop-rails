d7707f6ac8636f06c192a191e35cb19ea37349ff
James Zhan <zhiqiangzhan@gmail.com>
2013-04-04 00:16:39 +0800

    Init Commit.

---
+++ lib/jquery/crop/rails.rb
@@ -0,0 +1,9 @@
+require "jquery/crop/rails/version"
+
+module Jquery
+  module Crop
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jquery/crop/rails/version.rb
@@ -0,0 +1,7 @@
+module Jquery
+  module Crop
+    module Rails
+      VERSION = "0.0.1"
+    end
+  end
+end
